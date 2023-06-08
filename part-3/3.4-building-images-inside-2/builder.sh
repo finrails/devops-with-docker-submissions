336734b721f326208f4a9b97e287cc017f487c56
Fin <desenvolvedor.vitorsouza@gmail.com>
2023-06-07 22:55:32 -0400

    add: part 3, exercise 4

---
+++ part-3/3.4-building-images-inside-2/builder.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+repo_path=$1
+
+docker_repo=$2
+
+base=https://github.com/
+
+github_current_uri=$base$repo_path
+
+echo $github_current_uri
+
+git clone $github_current_uri
+
+user=$(echo $repo_path | cut -d "/" -f 1)
+repo=$(echo $repo_path | cut -d "/" -f 2)
+
+cd $repo
+
+docker login -u $DOCKER_USER -p $DOCKER_PWD
+
+docker build . -t $docker_repo && docker push $docker_repo:latest && cd .. && rm -rf $repo
+
+echo "Done...!"
